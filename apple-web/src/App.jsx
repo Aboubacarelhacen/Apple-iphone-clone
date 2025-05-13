ac54f4c5254155474c205e3701530b7565bd8b54
Aboubacarelhacen <136017245+Aboubacarelhacen@users.noreply.github.com>
2025-05-14 02:30:27 +0300

    Navbar created

---
+++ apple-web/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Highlight from './components/Highlight'
+
+
+const App = () => {
+ 
+
+  return (
+    <main className='bg-black'>
+      <Navbar />
+      <Hero />
+      <Highlight />
+    </main>
+   
+  )
+}
+
+export default App
---
+++ apple-web/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='w-full nav-height bg-black relative'>
+      <div className='h-5/6 flex-center flex-col'>
+        <p>iphone 15</p>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
